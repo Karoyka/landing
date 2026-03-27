aa8c5067a8a3c66f6f2b41f4d5de2bc1af3b3896
matsiushM <matiush2018@yandex.ru>
2026-03-27 16:20:21 +0300

    Fix image paths after removing basePath

--- imageLoader.ts
+++ imageLoader.ts
@@ -1,4 +1,3 @@
 export default function imageLoader({ src }: { src: string }) {
-    const prefix = process.env.NODE_ENV === 'production' ? '/landing' : '';
-    return `${prefix}${src}`;
+    return src;
 }
